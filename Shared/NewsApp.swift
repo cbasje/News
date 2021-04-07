3dc627c8739392ebf61ff2cf49012b748209f0b7
sebastiaanb <45468731+sebastiaanb@users.noreply.github.com>
2021-04-07 14:59:42 +0100

    Initial Commit

---
+++ Shared/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  Shared
+//
+//  Created by benjamiin on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
